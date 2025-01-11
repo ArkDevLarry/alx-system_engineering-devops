08587a93f21d4ab4b5e7901e1415c4f6572edd99
Arkdevlarry <akereleayomide106@gmail.com>
2025-01-10 23:16:44 -0800

    try now

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # Using Puppet, install flask from pip3
-exec { 'install_flask_and_werkzeug':
-  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
+package { 'werkzeug':
+  ensure   => '2.0.3',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
