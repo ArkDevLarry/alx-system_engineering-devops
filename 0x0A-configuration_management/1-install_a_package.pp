654aa7dfd95b2b5f90d360aced55d4b3e0798829
Arkdevlarry <akereleayomide106@gmail.com>
2025-01-10 23:18:15 -0800

    Pusher

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  require => Package['python3-pip'],
 }
-
-package { 'werkzeug':
-  ensure   => '2.0.3',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
